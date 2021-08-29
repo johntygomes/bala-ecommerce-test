fed645b49af679f9e6a39aeab63841066f8a95ea
Balakishan Renjarla <59106701+balakishanR@users.noreply.github.com>
2021-08-29 14:41:44 +0530

    push

--- BYC/settings.py
+++ BYC/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,8 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 # Managing media
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
-MEDIA_URL = "/image/download/"
-MEDIA_ROOT = BASE_DIR
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = "/uploads/products/"
+MEDIA_ROOT = os.path.join(BASE_DIR, '/uploads/products')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/uploads/products'),
+]
