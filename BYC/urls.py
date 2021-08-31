2fb5c5ec5689b484de4d771877eb8f848aae1e21
Balakishan Renjarla <59106701+balakishanR@users.noreply.github.com>
2021-08-31 12:24:58 +0530

    PUSH

--- BYC/urls.py
+++ BYC/urls.py
@@ -1,18 +1,9 @@
-
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.urls import path, include, re_path
-from django.views.static import serve
+from django.urls import path  , include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
-    re_path(r'^uploads/products/(?P<path>.*)$', serve, {'document_root' : settings.MEDIA_ROOT}),
+    path('' , include('store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-#if settings.DEBUG:
-#urlpatterns += static(settings.STATIC_URL, document_root = settings.STATICFILES_DIRS[0])
-#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
